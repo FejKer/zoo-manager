4af428435a068dee0718f6d936f18e777d86b627
Oskar Migowski <o.migowski@protonmail.com>
2022-12-16 00:17:34 +0100

    Implemented AnimalModelAssembler to make data more RESTful

--- src/main/java/me/omigo/zoomanager/AnimalController.java
+++ src/main/java/me/omigo/zoomanager/AnimalController.java
@@ -1,23 +1,35 @@
 package me.omigo.zoomanager;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class AnimalController {
 
     private final AnimalRepository animalRepository;
+    private final AnimalModelAssembler animalModelAssembler;
 
-    @Autowired
-    public AnimalController(AnimalRepository animalRepository) {
+    public AnimalController(AnimalRepository animalRepository, AnimalModelAssembler animalModelAssembler) {
         this.animalRepository = animalRepository;
+        this.animalModelAssembler = animalModelAssembler;
     }
 
     @GetMapping("/animals")
     public List<Animal> getAllAnimals() {
         return animalRepository.findAll();
+    }
+
+    @GetMapping("/animals/{id}")
+    public EntityModel<Animal> one(@PathVariable Long id) {
+        Animal animal = animalRepository.findById(id).orElseThrow(() -> new AnimalNotFoundException(id));
+
+        return animalModelAssembler.toModel(animal);
     }
 
     @PostMapping("/animals")
---
+++ src/main/java/me/omigo/zoomanager/AnimalModelAssembler.java
@@ -0,0 +1,24 @@
+package me.omigo.zoomanager;
+
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class AnimalModelAssembler implements RepresentationModelAssembler<Animal, EntityModel<Animal>> {
+    @Override
+    public EntityModel<Animal> toModel(Animal animal) {
+        return EntityModel.of(animal,
+                linkTo(methodOn(AnimalController.class).one(animal.getId())).withSelfRel(),
+                linkTo(methodOn(AnimalController.class).getAllAnimals()).withRel("employees"));
+    }
+
+    @Override
+    public CollectionModel<EntityModel<Animal>> toCollectionModel(Iterable<? extends Animal> entities) {
+        return RepresentationModelAssembler.super.toCollectionModel(entities);
+    }
+}
