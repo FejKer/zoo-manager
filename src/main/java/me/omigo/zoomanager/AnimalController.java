8ea8d7487c6ac53c4c60a69c54f056fc983e15c3
Oskar Migowski <o.migowski@protonmail.com>
2022-12-16 18:31:52 +0100

    Changed search method to get Animals by name

--- src/main/java/me/omigo/zoomanager/AnimalController.java
+++ src/main/java/me/omigo/zoomanager/AnimalController.java
@@ -24,14 +24,13 @@ public class AnimalController {
 
     @GetMapping("/animals")
     public CollectionModel<EntityModel<Animal>> getAllAnimals() {
-        List<EntityModel<Animal>> animals = animalRepository.findAll().stream() //
-                .map(animalModelAssembler::toModel) //
-                .collect(Collectors.toList());
+        List<EntityModel<Animal>> animals = animalRepository.findAll().stream()
+                .map(animalModelAssembler::toModel).collect(Collectors.toList());
 
         return CollectionModel.of(animals, linkTo(methodOn(AnimalController.class).getAllAnimals()).withSelfRel());
     }
 
-    @GetMapping("/animals/{id}")
+    @GetMapping("/animals/id/{id}")
     public EntityModel<Animal> getOneAnimal(@PathVariable Long id) {
         Animal animal = animalRepository.findById(id).orElseThrow(() -> new AnimalNotFoundException(id));
 
@@ -40,14 +39,8 @@ public class AnimalController {
 
     @GetMapping("/animals/name/{name}")
     public CollectionModel<EntityModel<Animal>> getOneAnimal(@PathVariable String name) {
-        List<EntityModel<Animal>> animals = new ArrayList<>();
-        for (Animal a:
-             animalRepository.findAll()) {
-            if (a.getName().equals(name)) {
-                animals.add(animalModelAssembler.toModel(a));
-            }
-        }
-        if (animals.size() == 0) throw new AnimalNotFoundException(name);
+        List<EntityModel<Animal>> animals = animalRepository.findByName(name).stream()
+                .map(animalModelAssembler::toModel).collect(Collectors.toList());
 
         return CollectionModel.of(animals, linkTo(methodOn(AnimalController.class).getOneAnimal(name)).withRel("animals " + name));
     }
@@ -57,5 +50,4 @@ public class AnimalController {
         return animalRepository.save(animal);
     }
 
-    // other endpoints
 }
--- src/main/java/me/omigo/zoomanager/AnimalRepository.java
+++ src/main/java/me/omigo/zoomanager/AnimalRepository.java
@@ -3,6 +3,9 @@ package me.omigo.zoomanager;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AnimalRepository extends JpaRepository<Animal, Long> {
+    List<Animal> findByName(String name);
 }
