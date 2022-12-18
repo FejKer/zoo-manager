3086f90eaea1cd209a3a4d1759f6a216c1255a19
Oskar Migowski <o.migowski@protonmail.com>
2022-12-18 07:06:07 +0100

    Created test methods

--- src/test/java/me/omigo/zoomanager/ZooManagerApplicationTests.java
+++ src/test/java/me/omigo/zoomanager/ZooManagerApplicationTests.java
@@ -1,13 +1,63 @@
 package me.omigo.zoomanager;
 
+import me.omigo.zoomanager.entities.Animal;
+import me.omigo.zoomanager.repositories.AnimalRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+        classes = ZooManagerApplication.class)
+@AutoConfigureMockMvc
 class ZooManagerApplicationTests {
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private AnimalRepository repository;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void givenAnimals_whenGetAnimals_thenStatusOk() throws Exception {
+        mvc.perform(get("/animals")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(new MediaType("application", "*+json")));
+    }
+
+    @Test
+    void givenAnimals_whenGetAnimals_thenFirstAnimalNameIsBilbo() throws Exception {
+        Optional<Animal> testAnimal = repository.findById(1L);
+        assertThat(testAnimal.get().getName()).isEqualTo("Bilbo");          //testing if preloaded animals are correct
+    }
+
+    @Test
+    void givenAnimals_whenCreateAnimals_thenStatusOk() throws Exception {
+        mvc.perform(post("/animals")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                                "name": "TestElephant",
+                                                "species": "Elephant",
+                                                "zone": "z1"
+                                }"""))
+                .andExpect(status().isOk())
+                .andExpect(content().string("{\"id\":5,\"name\":\"TestElephant\",\"species\":\"Elephant\",\"requiredFood\":20,\"zoneName\":\"z1\"}"))
+                .andReturn();           //test if creating animal returns animal body
     }
 
 }
